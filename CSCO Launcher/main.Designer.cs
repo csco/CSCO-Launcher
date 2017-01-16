adbda90e3949ea2adeb974a2dad3cc3d49da736e
Bluscream <admin@timo.de.vc>
2017-01-16 18:00:24 +0100

    Add project files.

---
+++ CSCO Launcher/main.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace CSCO_Launcher
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.webBrowser3 = new System.Windows.Forms.WebBrowser();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(943, 553);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webBrowser1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(935, 527);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "News";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(929, 521);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("http://steamcommunity.com/groups/classicoffensive#announcements", System.UriKind.Absolute);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.webBrowser3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(935, 527);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Servers";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.webBrowser2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(935, 527);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Issues";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser2.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.ScriptErrorsSuppressed = true;
+            this.webBrowser2.Size = new System.Drawing.Size(935, 527);
+            this.webBrowser2.TabIndex = 0;
+            this.webBrowser2.Url = new System.Uri("http://github.com/csco/Classic-Offensive/issues", System.UriKind.Absolute);
+            this.webBrowser2.WebBrowserShortcutsEnabled = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(872, 578);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 578);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(741, 577);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Check for Updates";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(94, 577);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Settings";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser3
+            // 
+            this.webBrowser3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser3.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser3.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser3.Name = "webBrowser3";
+            this.webBrowser3.ScriptErrorsSuppressed = true;
+            this.webBrowser3.Size = new System.Drawing.Size(929, 521);
+            this.webBrowser3.TabIndex = 0;
+            this.webBrowser3.Url = new System.Uri("http://onepointsix.org/", System.UriKind.Absolute);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(935, 527);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Infos";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 613);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "main";
+            this.Text = "Counter-Strike: Classic Offensive Launcher";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.WebBrowser webBrowser3;
+        private System.Windows.Forms.TabPage tabPage4;
+    }
+}
+
---
+++ CSCO Launcher/main.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Windows.Forms;
+using SteamKit2;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading;
+using static SteamKit2.SteamMasterServer;
+using System.Net;
+using IpPublicKnowledge;
+using System.Collections.Generic;
+using static SteamKit2.CDNClient;
+
+namespace CSCO_Launcher
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.FirstStart) { settings settingsForm = new settings(); settingsForm.Show(); }
+        }
+        public class SteamListConnection
+        {
+            SteamClient steamClient;
+            SteamMasterServer masterServer;
+            CallbackManager manager;
+            SteamUser steamUser;
+
+            List<Server> serverList = new List<Server>();
+
+            bool isRunning;
+
+            string username;
+            string password;
+            string authCode, twoFactorAuth;
+
+
+            public SteamListConnection(List<Server> serverList, string username, string password)
+            {
+                this.serverList = serverList;
+                this.username = username;
+                this.password = password;
+
+                steamClient = new SteamClient();
+                manager = new CallbackManager(steamClient);
+                steamUser = steamClient.GetHandler<SteamUser>();
+
+                manager.Subscribe<SteamClient.ConnectedCallback>(OnConnected);
+                manager.Subscribe<SteamClient.DisconnectedCallback>(OnDisconnected);
+
+                manager.Subscribe<SteamUser.LoggedOnCallback>(OnLoggedOn);
+
+                isRunning = true;
+
+                steamClient.Connect();
+
+                while (isRunning)
+                {
+                    manager.RunWaitCallbacks(TimeSpan.FromSeconds(1));
+                }
+
+            }
+
+
+            void OnConnected(SteamClient.ConnectedCallback callback)
+            {
+                if (callback.Result != EResult.OK)
+                {
+                    isRunning = false;
+                    return;
+                }
+
+
+                byte[] sentryHash = null;
+                if (File.Exists("sentry.bin"))
+                {
+                    byte[] sentryFile = File.ReadAllBytes("sentry.bin");
+                    sentryHash = CryptoHelper.SHAHash(sentryFile);
+                }
+
+                Console.WriteLine("Username: " + this.username + " | password: " + this.password);
+                steamUser.LogOn(new SteamUser.LogOnDetails
+                {
+                    Username = this.username,
+                    Password = this.password,
+                    AuthCode = authCode,
+                    TwoFactorCode = twoFactorAuth,
+                    SentryFileHash = sentryHash,
+                });
+            }
+
+            void OnDisconnected(SteamClient.DisconnectedCallback callback)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                steamClient.Connect();
+            }
+
+            void OnLoggedOn(SteamUser.LoggedOnCallback callback)
+            {
+                bool isSteamGuard = callback.Result == EResult.AccountLogonDenied;
+                bool is2FA = callback.Result == EResult.AccountLoginDeniedNeedTwoFactor;
+                Console.WriteLine("Logged in...");
+                if (isSteamGuard || is2FA)
+                {
+                    Console.WriteLine("Creating message box");
+                    //AuthCodeDisplay messageBox = new AuthCodeDisplay();
+                    //Application.Run(messageBox);
+                    //messageBox.WaitForSubmission();
+
+                    //if (is2FA)
+                    //{
+                    //    twoFactorAuth = messageBox.GetAuthCode();
+                    //    messageBox.Close();
+
+                    //}
+                    //else
+                    //{
+                    //    authCode = messageBox.GetAuthCode();
+                    //}
+
+                    //messageBox.Close();
+
+                    return;
+                }
+
+                if (callback.Result != EResult.OK)
+                {
+                    isRunning = false;
+                    return;
+                }
+
+                Console.WriteLine("Passed 2fa");
+                Console.WriteLine("Starting server list grab");
+                StartServerListGrab();
+
+            }
+
+            void OnMachineAuth(SteamUser.UpdateMachineAuthCallback callback)
+            {
+                int fileSize;
+                byte[] sentryHash;
+                using (var fs = File.Open("sentry.bin", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    fs.Seek(callback.Offset, SeekOrigin.Begin);
+                    fs.Write(callback.Data, 0, callback.BytesToWrite);
+                    fileSize = (int)fs.Length;
+
+                    fs.Seek(0, SeekOrigin.Begin);
+                    using (var sha = new SHA1CryptoServiceProvider())
+                    {
+                        sentryHash = sha.ComputeHash(fs);
+                    }
+                }
+
+                steamUser.SendMachineAuthResponse(new SteamUser.MachineAuthDetails
+                {
+                    JobID = callback.JobID,
+
+                    FileName = callback.FileName,
+
+                    BytesWritten = callback.BytesToWrite,
+                    FileSize = fileSize,
+                    Offset = callback.Offset,
+
+                    Result = EResult.OK,
+                    LastError = 0,
+
+                    OneTimePassword = callback.OneTimePassword,
+
+                    SentryFileHash = sentryHash,
+                });
+
+                StartServerListGrab();
+            }
+
+            async void StartServerListGrab()
+            {
+                this.masterServer = steamClient.GetHandler<SteamMasterServer>();
+
+                QueryDetails querydetails = new QueryDetails();
+                querydetails.AppID = 740;
+                //querydetails.GeoLocatedIP = this.GetExternalIP();
+                //querydetails.Filter = "\\gamedir\\csco";
+
+
+                QueryCallback query = await masterServer.ServerQuery(querydetails);
+
+                foreach (QueryCallback.Server serv in query.Servers)
+                {
+                    Console.WriteLine(serv.EndPoint.Address.ToString() + ":" + serv.EndPoint.Port);
+                }
+
+            }
+
+            public IPAddress GetExternalIP()
+            {
+                return IPK.GetMyPublicIp();
+            }
+
+        }
+    }
+}
